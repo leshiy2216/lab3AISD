0d4021cf9b5525d84403d932199a658f47e967ae
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-12-18 14:21:35 +0400

    bubble sort2

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,20 +1,42 @@
-﻿// lab3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
 
-#include <iostream>
+struct stats {
+    size_t comparison_count = 0;
+    size_t copy_count = 0;
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void bubbleSort(std::vector<int>& arr, stats& statistics) {
+    size_t n = arr.size();
+    bool swapped;
+    for (size_t i = 0; i < n - 1; ++i) {
+        swapped = false;
+        for (size_t j = 0; j < n - i - 1; ++j) {
+            ++statistics.comparison_count;
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                ++statistics.copy_count;
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main() {
+    setlocale(LC_ALL, "Rus");
+    std::vector<int> array = { 64, 34, 25, 12, 22, 11, 90 };
+    stats statistics;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    bubbleSort(array, statistics);
+    for (const auto& element : array) {
+        std::cout << element << " ";
+    }
+    std::cout << "Количество сравнений: " << statistics.comparison_count << "\n";
+    std::cout << "Количество копирований: " << statistics.copy_count << "\n";
+
+    return 0;
+}
